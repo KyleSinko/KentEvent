80ba530853e0773d17d8734e2d7548fa06da791b
cdegeorge <connorjdeg@gmail.com>
2019-03-14 18:05:37 -0400

    Implimented Bootstrap

---
+++ Flask/flaskr/user.py
@@ -0,0 +1,12 @@
+﻿from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def Index():
+	return render_template('/user/index.html')
+
+if __name__ == '__main__':
+	app.run(debug = True)
